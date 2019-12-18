73aeb4334d042541bd7d06c5998a81d59073c9cd
Sp3k8eRX <sp3k8erx@gmail.com>
2019-12-18 20:21:05 +0200

    Done Exercises

--- Exercises/1-hoisting.js
+++ Exercises/1-hoisting.js
@@ -1,5 +1,11 @@
 'use strict';
 
-const fn = null;
+const fn = function (a) {
+  return a;
+
+};
+
+
+
 
 module.exports = { fn };
--- Exercises/4-count-types.js
+++ Exercises/4-count-types.js
@@ -1,5 +1,14 @@
 'use strict';
 
-const countTypesInArray = null;
+
+const countTypesInArray = arr => {
+  const collect = {};
+  for (const value of arr) {
+    const type = typeof value;
+    collect[type] = ++collect[type] || 1;
+  }
+  return collect;
+};
+
 
 module.exports = { countTypesInArray };
